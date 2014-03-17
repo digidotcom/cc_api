ed36d46856ae9e750aa9a2591ff1234c532ce10a
Hector Bujanda <hector.bujanda@digi.com>
2014-03-17 14:08:51 +0100

    Layer2 Digi Controlled defines, Match Layer2 User Controlled defines to Layer1 defines

--- source/ccapi_connector_config.h
+++ source/ccapi_connector_config.h
@@ -10,8 +10,18 @@
 
 #include "custom/custom_connector_config.h"
 
-/* if implementer does not define CCIMP_DEBUG_ENABLED, there won't be debugging in any layer */
+/* +++ Layer2 Digi Controlled defines +++ */
+
+#undef CONNECTOR_SM_UDP_MAX_SESSIONS
+#undef CONNECTOR_SM_SMS_MAX_SESSIONS
+
+/* --- Layer2 Digi Controlled defines --- */
+
+
+/* +++ Match Layer2 User Controlled defines to Layer1 defines +++ */
+
 #ifdef CCIMP_DEBUG_ENABLED
+/* if implementer does not define CCIMP_DEBUG_ENABLED, there won't be debugging in any layer */
 #define CONNECTOR_DEBUG
 #endif
 
@@ -22,5 +32,7 @@
 #ifdef CCIMP_SM_SMS_MAX_RX_SEGMENTS
 #define CONNECTOR_SM_SMS_MAX_RX_SEGMENTS CCIMP_SM_SMS_MAX_RX_SEGMENTS
 #endif
+
+/* --- Match Layer2 User Controlled defines to Layer1 defines --- */
 
 #endif /* _CCAPI_CONNECTOR_CONFIG_H_ */
