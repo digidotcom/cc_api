0bf12b374d5360a2330d3c05317182982d402062
Hector Bujanda <hector.bujanda@digi.com>
2014-03-28 11:43:05 +0100

    First draft of ccapi_start_stop app based on layer1 samples makefile

---
+++ tests/samples/ccapi_start_stop/main.c
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2014 Digi International Inc.,
+ * All rights not expressly granted are reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Digi International Inc. 11001 Bren Road East, Minnetonka, MN 55343
+ * =======================================================================
+ */
+#include "ccapi/ccapi.h"
+
+#define DEVICE_TYPE_STRING      "Device type"
+#define DEVICE_CLOUD_URL_STRING "login.etherios.com"
+
+void fill_start_structure_with_good_parameters(ccapi_start_t * start)
+{
+    uint8_t device_id[DEVICE_ID_LENGTH] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x9D, 0xFF, 0xFF, 0xAB, 0xCD, 0xEF};
+    char const * const device_cloud_url = DEVICE_CLOUD_URL_STRING;
+    char const * const device_type = DEVICE_TYPE_STRING;
+    start->vendor_id = 0x12345678; /* Set vendor_id or ccapi_init_error_invalid_vendorid will be returned instead */
+    memcpy(start->device_id, device_id, sizeof start->device_id);
+    start->device_cloud_url = device_cloud_url;
+    start->device_type = device_type;
+
+    start->service.cli = NULL;
+    start->service.receive = NULL;
+    start->service.file_system = NULL;
+    start->service.firmware = NULL;
+    start->service.rci = NULL;
+}
+
+int main (void)
+{
+    ccapi_start_t start = {0};
+    ccapi_start_error_t start_error = CCAPI_START_ERROR_NONE;
+    ccapi_stop_error_t stop_error = CCAPI_STOP_ERROR_NONE;
+
+    fill_start_structure_with_good_parameters(&start);
+
+    start_error = ccapi_start(&start);
+
+    if (start_error == CCAPI_START_ERROR_NONE)
+    {
+        printf("ccapi_start success\n");
+    }
+    else
+    {
+        printf("ccapi_start error %d\n", start_error);
+    }
+    stop_error = ccapi_stop(CCAPI_STOP_IMMEDIATELY);
+
+    if (stop_error == CCAPI_STOP_ERROR_NONE)
+    {
+        printf("ccapi_stop success\n");
+    }
+    else
+    {
+        printf("ccapi_stop error %d\n", stop_error);
+    }
+    return start_error | stop_error;
+}
