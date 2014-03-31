1837e32beb1e72bdc37949fef93111236de2aef9
Sebastian Pastor <sebastian.pastor@digi.com>
2014-03-31 22:32:06 +0200

    TCP start: sanity checks, default values and values copy to internal structure.

--- include/ccxapi/ccxapi_init.h
+++ include/ccxapi/ccxapi_init.h
@@ -10,5 +10,6 @@
 
 ccapi_start_error_t ccxapi_start(ccapi_handle_t * const ccapi_handle, ccapi_start_t const * const start);
 ccapi_stop_error_t ccxapi_stop(ccapi_handle_t const ccapi_handle, ccapi_stop_t const behavior);
+ccapi_tcp_start_error_t ccxapi_start_transport_tcp(ccapi_handle_t const ccapi_handle, ccapi_tcp_info_t const * const tcp_start);
 
 #endif
