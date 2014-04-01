241a95d2ce9ca1ba34216154be6b69d8d8b4f867
Sebastian Pastor <sebastian.pastor@digi.com>
2014-04-01 18:36:05 +0200

    Mocks: mocks.cpp was not starting/stopping connector_initiate_action() mock.

--- tests/unit_tests/mocks/mocks.cpp
+++ tests/unit_tests/mocks/mocks.cpp
@@ -19,7 +19,7 @@ void Mock_create_all()
     Mock_ccimp_create_thread_create();
     Mock_connector_init_create();
     Mock_connector_run_create();
-    Mock_connector_init_create();
+    Mock_connector_initiate_action_create();
     Mock_ccimp_network_tcp_open_create();
     Mock_ccimp_network_tcp_send_create();
     Mock_ccimp_network_tcp_receive_create();
@@ -43,7 +43,7 @@ void Mock_destroy_all()
     Mock_ccimp_create_thread_destroy();
     Mock_connector_init_destroy();
     Mock_connector_run_destroy();
-    Mock_connector_init_destroy();
+    Mock_connector_initiate_action_destroy();
     Mock_ccimp_network_tcp_open_destroy();
     Mock_ccimp_network_tcp_send_destroy();
     Mock_ccimp_network_tcp_receive_destroy();
