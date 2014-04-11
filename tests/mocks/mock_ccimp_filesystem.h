bd913c255a01597ab62b9590df049974d0ffd732
Sebastian Pastor <sebastian.pastor@digi.com>
2014-04-11 19:31:24 +0200

    mocks: ccimp_fs added function prototypes for ccimp_fs_*_real() functions

--- tests/mocks/mock_ccimp_filesystem.h
+++ tests/mocks/mock_ccimp_filesystem.h
@@ -57,4 +57,21 @@ void Mock_ccimp_fs_session_error_create(void);
 void Mock_ccimp_fs_session_error_destroy(void);
 void Mock_ccimp_fs_session_error_expectAndReturn(ccimp_fs_session_error_t * expect, ccimp_status_t retval);
 
+extern "C" {
+ccimp_status_t ccimp_fs_file_open_real(ccimp_fs_file_open_t * const file_open_data);
+ccimp_status_t ccimp_fs_file_read_real(ccimp_fs_file_read_t * const file_read_data);
+ccimp_status_t ccimp_fs_file_write_real(ccimp_fs_file_write_t * const file_write_data) ;
+ccimp_status_t ccimp_fs_file_close_real(ccimp_fs_file_close_t * const file_close_data);
+ccimp_status_t ccimp_fs_file_seek_real(ccimp_fs_file_seek_t * const file_seek_data);
+ccimp_status_t ccimp_fs_file_truncate_real(ccimp_fs_file_truncate_t * const file_truncate_data);
+ccimp_status_t ccimp_fs_file_remove_real(ccimp_fs_file_remove_t * const file_remove_data);
+ccimp_status_t ccimp_fs_dir_open_real(ccimp_fs_dir_open_t * const dir_open_data);
+ccimp_status_t ccimp_fs_dir_read_entry_real(ccimp_fs_dir_read_entry_t * const dir_read_data);
+ccimp_status_t ccimp_fs_dir_entry_status_real(ccimp_fs_dir_entry_status_t * const dir_entry_status_data);
+ccimp_status_t ccimp_fs_dir_close_real(ccimp_fs_dir_close_t * const dir_close_data);
+ccimp_status_t ccimp_fs_hash_status_real(ccimp_fs_hash_status_t * const hash_status_data);
+ccimp_status_t ccimp_fs_hash_file_real(ccimp_fs_hash_file_t * const file_hash_data);
+ccimp_status_t ccimp_fs_error_desc_real(ccimp_fs_error_desc_t * const error_desc_data);
+ccimp_status_t ccimp_fs_session_error_real(ccimp_fs_session_error_t * const session_error_data);
+}
 #endif
