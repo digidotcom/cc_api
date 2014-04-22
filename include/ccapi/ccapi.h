458418b2600dd1053be4f50c448e866b8551cad5
Sebastian Pastor <sebastian.pastor@digi.com>
2014-04-22 14:44:52 +0200

    filesystem access callback definitions.
    	- CCAPI-32

--- include/ccapi/ccapi.h
+++ include/ccapi/ccapi.h
@@ -9,6 +9,8 @@
 #define _CCAPI_H_
 
 #include "ccimp/ccimp_types.h"
-#include "ccapi/ccapi_init.h"
 #include "ccapi/ccapi_transport_tcp.h"
+#include "ccapi/ccapi_filesystem.h"
+#include "ccapi/ccapi_init.h"
+
 #endif
---
+++ include/ccapi/ccapi_filesystem.h
@@ -0,0 +1,24 @@
+#ifndef _CCAPI_FILESYSTEM_H_
+#define _CCAPI_FILESYSTEM_H_
+
+typedef enum {
+  CCAPI_FS_REQUEST_READ,
+  CCAPI_FS_REQUEST_WRITE,
+  CCAPI_FS_REQUEST_READWRITE,
+  CCAPI_FS_REQUEST_REMOVE,
+  CCAPI_FS_REQUEST_LIST,
+  CCAPI_FS_REQUEST_UNKNOWN
+} ccapi_fs_request_t;
+
+typedef enum {
+    CCAPI_FS_ACCESS_DENY,
+    CCAPI_FS_ACCESS_ALLOW
+} ccapi_fs_access_t;
+
+typedef ccapi_fs_access_t (*ccapi_fs_access_cb_t)(char const * const local_path, ccapi_fs_request_t const request);
+
+typedef struct {
+    ccapi_fs_access_cb_t access_cb;
+} ccapi_filesystem_service_t;
+
+#endif
