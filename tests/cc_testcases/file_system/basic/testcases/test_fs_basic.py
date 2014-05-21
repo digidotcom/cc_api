8da195ddab7ef25545ab127fec559b7d519955b0
Arturo Buzarra <arturo.buzarra@digi.com>
2014-05-21 17:49:02 +0200

    cc_testcases: improve test cases in test_fs_basic.py
    Signed-off-by: Arturo Buzarra <arturo.buzarra@digi.com>

--- tests/cc_testcases/file_system/basic/testcases/test_fs_basic.py
+++ tests/cc_testcases/file_system/basic/testcases/test_fs_basic.py
@@ -23,7 +23,7 @@ for i in xrange(MAX_TEST_FILE_SIZE):
 class DiscoveryTestCase(cc_testcase.TestCase):
 
     def test_1_upload_file_execution_path(self):
-        """ Tests put and get on different sized files. This creates a series of files with random content that are used by other tests. """
+        """ Tests put and get on different sized files. This creates a series of files with random content in the execution path. """
         global FileData
         global FileList
 
@@ -41,7 +41,7 @@ class DiscoveryTestCase(cc_testcase.TestCase):
 
 
     def test_2_upload_file_root_path(self):
-        """ Tests put and get a file inside a subdirectory in the root path. """
+        """ Tests put and get a file inside the root path. """
         global FileData
 
         rootPath = self.tempPath
@@ -60,7 +60,7 @@ class DiscoveryTestCase(cc_testcase.TestCase):
 
 
     def test_3_upload_file_subdirectories_root_path(self):
-        """ Tests put and get a file inside a subdirectory in the root path. """
+        """ Tests put and get a file inside different subdirectory levels in the root path. """
         global FileData
 
         rootPath = self.tempPath
@@ -83,7 +83,7 @@ class DiscoveryTestCase(cc_testcase.TestCase):
 
 
     def test_4_upload_file_max_path_length(self):
-        """ Tests put and get a file inside a subdirectory in the root path. """
+        """ Tests put and get a file in a long path. """
         global FileData
 
         rootPath = self.tempPath
@@ -105,6 +105,170 @@ class DiscoveryTestCase(cc_testcase.TestCase):
 
         # Upload, Download and Verify each file
         self.auxiliar_upload_download_verify(filePath, fileSize, FileData)
+
+
+
+    def test_5_upload_file_folder_without_rights(self):
+        """ Tests put a file inside a subdirectory in the root path without write permissions. """
+        global FileData
+
+        # Generate the new path
+        rootPath = self.tempPath
+        rootPath = os.path.join( rootPath , "folderWithoutRights")
+
+
+        # Create the new directory on Device without write permissions
+        os.makedirs(rootPath, 0400)
+
+        # Generate the destination path for the file
+        fileName = "test_fs_basic_without_rights.txt"
+        filePath = os.path.join(rootPath,fileName)
+        fileSize = len(FileData)
+
+        self.log.info("Uploading file '%s' in a path without rights" % ( filePath ) )
+
+        # Send file to Device, expected result is error due to Permision denied
+        result, requestResponse = self.cloudHandler.uploadFileToDevice(self.device_id, filePath, FileData)
+        if (result):
+            self.fail("Could upload file '%s' on device in a folder without write permissions" % (filePath) )
+
+        else:
+            # Get error string
+            errorMessage = requestResponse.resource["sci_reply"]["file_system"]["device"]["commands"]["put_file"]["error"]['#text']
+
+            if( errorMessage == "Permission denied" ):
+                self.log.info("Device rejects the file '%s' due to '%s'....OK" % (fileName, errorMessage) )
+            else:
+                self.fail("Unknown error on device in a folder without write permissions: %s" % (requestResponse.content) )
+
+
+
+    def test_6_upload_file_unknown_folder(self):
+        """ Tests put a file in a nonexistent subdirectory. """
+        global FileData
+
+        # Generate the new path
+        rootPath = self.tempPath
+        rootPath = os.path.join( rootPath , "unknownFolder")
+
+        # Generate the destination path for the file
+        fileName = "test_fs_basic_unknown.txt"
+        filePath = os.path.join(rootPath,fileName)
+        fileSize = len(FileData)
+
+        self.log.info("Uploading file '%s' in a unknown path" % ( filePath ) )
+
+        # Send file to Device, expected result is error due to 'No such file or directory'
+        result, requestResponse = self.cloudHandler.uploadFileToDevice(self.device_id, filePath, FileData)
+        if (result):
+            self.fail("Could upload file '%s' on device in an unknown folder" % (filePath) )
+
+        else:
+            # Get error string
+            errorMessage = requestResponse.resource["sci_reply"]["file_system"]["device"]["commands"]["put_file"]["error"]['#text']
+
+            if( errorMessage == "No such file or directory" ):
+                self.log.info("Device rejects the file '%s' due to '%s'....OK" % (fileName, errorMessage) )
+            else:
+                self.fail("Unknown error on device in an unknown folder: %s" % (requestResponse.content) )
+
+
+
+    def test_7_download_file_unknown_folder(self):
+        """ Tests get a file from a nonexistent folder. """
+
+        # Generate the new path
+        rootPath = self.tempPath
+        rootPath = os.path.join( rootPath , "unknownFolder")
+
+        # Generate the destination path for the file
+        fileName = "test_fs_basic_unknown.txt"
+        filePath = os.path.join(rootPath,fileName)
+
+        self.log.info("Downloading file '%s' from a unknown path" % ( filePath ) )
+
+        # Recover file to Device, expected result is error due to Permision denied
+        result, eachFileDownloadData, requestResponse = self.cloudHandler.downloadFileFromDevice(self.device_id, filePath)
+        if (result):
+            self.fail("Could download file '%s' on device from an unknown folder" % (filePath) )
+
+        else:
+            # Get error string
+            errorMessage = requestResponse.resource["sci_reply"]["file_system"]["device"]["commands"]["get_file"]["error"]['#text']
+
+            if( errorMessage == "No such file or directory" ):
+                self.log.info("Device rejects the file '%s' due to '%s'....OK" % (fileName, errorMessage) )
+            else:
+                self.fail("Unknown error on device in an unknown folder: %s" % (requestResponse.content) )
+
+
+
+    def test_8_download_file_unknown(self):
+        """ Tests get a nonexistent file. """
+
+        # Generate the destination path for the file
+        fileName = "test_fs_basic_unknown.txt"
+        filePath = fileName
+
+        self.log.info("Downloading file '%s' from a unknown path" % ( filePath ) )
+
+        # Recover file to Device, expected result is error due to Permision denied
+        result, eachFileDownloadData, requestResponse = self.cloudHandler.downloadFileFromDevice(self.device_id, filePath)
+        if (result):
+            self.fail("Could download file '%s' on device from an unknown folder" % (filePath) )
+
+        else:
+            # Get error string
+            errorMessage = requestResponse.resource["sci_reply"]["file_system"]["device"]["commands"]["get_file"]["error"]['#text']
+
+            if( errorMessage == "No such file or directory" ):
+                self.log.info("Device rejects the file '%s' due to '%s'....OK" % (fileName, errorMessage) )
+            else:
+                self.fail("Unknown error on device in an unknown folder: %s" % (requestResponse.content) )
+
+
+
+
+    def test_9_download_file_without_read_rights(self):
+        """ Tests get a file without read permissions inside a subdirectory in the root path. """
+        global FileData
+
+        # Generate the new path
+        rootPath = self.tempPath
+        rootPath = os.path.join( rootPath , "folderWithRights")
+
+
+        # Create the new directory on Device
+        os.makedirs(rootPath)
+
+        # Generate the destination path for the file
+        fileName = "test_fs_basic_without_rights.txt"
+        filePath = os.path.join(rootPath,fileName)
+        fileSize = len(FileData)
+
+        # Generate the file on Device
+        handlerFile = open(filePath, "w")
+        handlerFile.write(FileData)
+        handlerFile.close()
+
+        # Remove read rights from file
+        os.chmod(filePath, 0)
+
+        self.log.info("Downloading file '%s' from a path without read rights" % ( filePath ) )
+
+        # Recover file to Device, expected result is error due to Permision denied
+        result, eachFileDownloadData, requestResponse = self.cloudHandler.downloadFileFromDevice(self.device_id, filePath)
+        if (result):
+            self.fail("Could download file '%s' from device in a folder without read permissions" % (filePath) )
+
+        else:
+            # Get error string
+            errorMessage = requestResponse.resource["sci_reply"]["file_system"]["device"]["commands"]["get_file"]["error"]['#text']
+
+            if( errorMessage == "Permission denied" ):
+                self.log.info("Device rejects read the file '%s' due to '%s'....OK" % (fileName, errorMessage) )
+            else:
+                self.fail("Unknown error on device in a folder without write permissions: %s" % (requestResponse.content) )
 
 
 
