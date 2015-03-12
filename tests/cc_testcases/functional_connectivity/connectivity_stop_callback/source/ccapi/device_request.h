a6f61c2f75b4d4e25331c5c3e559c600e0dd8cf4
Arturo Buzarra <arturo.buzarra@digi.com>
2015-03-12 13:28:37 +0100

    [cc_testharness] add new test cases for connectivity_stop_callback
    Signed-off-by: Arturo Buzarra <arturo.buzarra@digi.com>

---
+++ tests/cc_testcases/functional_connectivity/connectivity_stop_callback/source/ccapi/device_request.h
@@ -0,0 +1,32 @@
+/*
+* Copyright (c) 2014 Etherios, a Division of Digi International, Inc.
+* All rights not expressly granted are reserved.
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this file,
+* You can obtain one at http://mozilla.org/MPL/2.0/.
+*
+* Etherios 11001 Bren Road East, Minnetonka, MN 55343
+* =======================================================================
+*/
+
+
+/* Device request */
+extern void ccapi_device_request_status_callback(char const * const target, ccapi_transport_t const transport, ccapi_buffer_info_t * const response_buffer_info, ccapi_receive_error_t receive_error);
+extern void ccapi_device_request_data_callback(char const * const target, ccapi_transport_t const transport, ccapi_buffer_info_t const * const request_buffer_info, ccapi_buffer_info_t * const response_buffer_info);
+extern ccapi_bool_t ccapi_device_request_accept_callback(char const * const target, ccapi_transport_t const transport);
+extern void fill_device_request_service(ccapi_start_t * start);
+
+
+extern connector_bool_t flag_start_tcp_transport;
+
+
+typedef enum {
+    TEST_01_START_FLAG,
+    TEST_02_START_FLAG,
+    TEST_04_START_FLAG,
+    TEST_06_START_FLAG,
+    TEST_DO_NOTHING_FLAG
+} test_flag_start_action_t;
+
+extern test_flag_start_action_t device_request_start_test_action;
